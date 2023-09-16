39f465b63b728658f61c7a0aaa392d0fcb67b49e
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-16 16:05:17 +0530

    commit10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import HomePage from "./Routes/Home";
+import About from './Routes/About';
 import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,19 +1,19 @@
 import NavBar from "../Components/Nav-Bar/NavBar";
 import Hero from "../Components/Hero-Section/Hero";
-import HomePagePic from "../Assets/Home.jpg"
+import HomePagePic from "../Assets/HomePage.jpg"
 
 function Home(){
     return (
         <>
         <NavBar />
         <Hero
-            cName="hero"
-            heroImage={HomePagePic}
-            title="Begin your journey With Us"
-            text="Providing a better service for you"
-            buttonText="Official GitHub "
-            btnClass="hide"
-             />
+        cName="hero"
+        heroImage={HomePagePic}
+        title="Hi Developer !"
+        text="Build your dream project with us."
+        url="https://github.com/huzaifameer"
+        btnClass="hide"
+        />
         </>
     );
     
