5fa42a2c8e46af5d15237fda2610e3b2350aba37
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-09-23 00:16:45 +0530

    commit

---
+++ src/Components/Name-Intro/Name-Anim.js
@@ -0,0 +1,9 @@
+import "./Name-Anim-styles.css"
+import {SmokePlay} from "../../Assets/smoke.mp4"
+
+function NameAnim(){
+    return(
+        <section></section>
+    )
+}
+export default NameAnim;
